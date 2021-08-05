b886127ef41d966352144813d94ca5626c1874ff
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-05 12:10:43 +0430

    Create s-2_p.py

---
+++ s-2_p.py
@@ -0,0 +1,5 @@
+n1 = int(input('adadeto begoo '))
+n2 = int(input('adade baaditm bgoo'))
+n3 = int(input('adade baditm begoo'))
+if (n1+n2)>n3 and (n3+n2)>n1 and (n1+n3) :
+    print(' mishe mosalas sakht')
