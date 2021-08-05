4ad77d9397d9316a83e34ac26cf68f7da200925d
Mrga637 <87478475+Mrga637@users.noreply.github.com>
2021-08-05 12:51:25 +0430

    Create s2_p2.py

---
+++ s2_p2.py
@@ -0,0 +1,8 @@
+name = input('esmet chie? ').lower()
+sen = int(input("chand salete? "))
+if name == 'ali' and sen > 30 :
+    print('bia to hale')
+elif name != 'ali' and  sen < 20 :
+    print('bia to ')
+else :
+    print('sorry babe')
